9e79dcb292751757339e1ee8fbb42a7a7a6825ec
Yurii Cherniak <juracherniak@gmail.com>
2023-07-05 23:27:19 +0200

    move to xor model

--- synapse.c
+++ synapse.c
@@ -3,37 +3,46 @@
 #define SYNAPSE_IMPLEMENTATION ;
 #include "synapse.h"
 
+typedef struct
+{
+    Mat a0;
+    Mat w1, b1, a1;
+    Mat w2, b2, a2;
+} XOR;
+
+float forward_xor(XOR m, float x1, float x2)
+{
+    MAT_AT(m.a0, 0, 0) = x1;
+    MAT_AT(m.a0, 0, 1) = x2;
+
+    mat_dot(m.a1, m.a0, m.w1);
+    mat_sum(m.a1, m.b1);
+    mat_sig(m.a1);
+
+    mat_dot(m.a2, m.a1, m.w2);
+    mat_sum(m.a2, m.b2);
+    mat_sig(m.a2);
+
+    return *m.a2.es;
+}
 int main()
 {
     srand(time(0));
 
-    Mat x = mat_alloc(1, 2);
+    XOR m;
 
-    Mat w1 = mat_alloc(2, 2);
-    Mat b1 = mat_alloc(1, 2);
-    Mat a1 = mat_alloc(1, 2);
+    m.a0 = mat_alloc(1, 2);
+    m.w1 = mat_alloc(2, 2);
+    m.b1 = mat_alloc(1, 2);
+    m.a1 = mat_alloc(1, 2);
+    m.w2 = mat_alloc(2, 1);
+    m.b2 = mat_alloc(1, 1);
+    m.a2 = mat_alloc(1, 1);
 
-    Mat w2 = mat_alloc(2, 1);
-    Mat b2 = mat_alloc(1, 1);
-    Mat a2 = mat_alloc(1, 1);
-
-    mat_rand(w1, -1, 1);
-    mat_rand(b1, -1, 1);
-    mat_rand(w2, -1, 1);
-    mat_rand(b2, -1, 1);
-
-    MAT_AT(x, 0, 0) = 0;
-    MAT_AT(x, 0, 1) = 1;
-
-    mat_dot(a1, x, w1);
-    mat_sum(a1, b1);
-    mat_sig(a1);
-
-    mat_dot(a2, a1, w2);
-    mat_sum(a2, b2);
-    mat_sig(a2);
-
-    MAT_PRINT(a2);
+    mat_rand(m.w1, -1, 1);
+    mat_rand(m.b1, -1, 1);
+    mat_rand(m.w2, -1, 1);
+    mat_rand(m.b2, -1, 1);
 
     return 0;
 }
