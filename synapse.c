d3dd5d0dd35ac8fd6599cd41fcf7a46c9ba0d6aa
Yurii Cherniak <juracherniak@gmail.com>
2023-07-05 16:04:24 +0200

    add dot product

--- synapse.c
+++ synapse.c
@@ -6,15 +6,16 @@
 int main()
 {
     srand(time(0));
-    Mat a = mat_alloc(2, 2);
-    mat_rand(a, 1, 2);
+    Mat a = mat_alloc(2, 3);
+    mat_fill(a, 1);
     mat_print(a);
-
-    Mat b = mat_alloc(2, 2);
-    mat_rand(b, 1, 2);
+    printf("-------\n");
+    Mat b = mat_alloc(3, 2);
+    mat_fill(b, 1);
     mat_print(b);
-
-    mat_sum(a, b);
-    mat_print(a);
+    printf("-------\n");
+    Mat dist = mat_alloc(2, 2);
+    mat_dot(dist, a, b);
+    mat_print(dist);
     return 0;
 }
--- synapse.h
+++ synapse.h
@@ -31,6 +31,7 @@ void mat_dot(Mat dist, Mat a, Mat b);
 void mat_sum(Mat dist, Mat a);
 void mat_print(Mat m);
 void mat_rand(Mat m, float low, float high);
+void mat_fill(Mat m, float x);
 
 #endif // SYNAPSE_H_
 
@@ -53,10 +54,27 @@ Mat mat_alloc(size_t rows, size_t cols)
 
 void mat_dot(Mat dist, Mat a, Mat b)
 {
+    S_ASSERT(a.cols == b.rows);
+    S_ASSERT(dist.rows == a.rows);
+    S_ASSERT(dist.cols == b.cols);
+
+    for (size_t i = 0; i < dist.rows; i++)
+    {
+        for (size_t j = 0; j < dist.cols; j++)
+        {
+            MAT_AT(dist, i, j) = 0;
+            for (size_t k = 0; k < a.cols; k++)
+            {
+                MAT_AT(dist, i, j) += MAT_AT(a, i, k) * MAT_AT(b, k, j);
+            }
+        }
+    }
+
     (void)dist;
     (void)a;
     (void)b;
 }
+
 void mat_sum(Mat dist, Mat a)
 {
     S_ASSERT(dist.rows == a.rows);
@@ -79,6 +97,17 @@ void mat_print(Mat m)
             printf("%f ", MAT_AT(m, i, j));
         }
         printf("\n");
+    }
+}
+
+void mat_fill(Mat m, float x)
+{
+    for (size_t i = 0; i < m.rows; i++)
+    {
+        for (size_t j = 0; j < m.cols; j++)
+        {
+            MAT_AT(m, i, j) = x;
+        }
     }
 }
 
