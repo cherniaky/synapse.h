fd708d104999e0da68ce2024de1045cf546dbc60
Yurii Cherniak <juracherniak@gmail.com>
2023-07-17 00:41:13 +0300

    rm comments

--- xor.gen.c
+++ xor.gen.c
@@ -18,19 +18,7 @@ int main()
         1,
         0,
     };
-    // size_t stride = 3;
-    // size_t n = sizeof(td) / sizeof(td[0]) / stride;
-    // Mat ti = {
-    //     .rows = n,
-    //     .cols = 2,
-    //     .stride = stride,
-    //     .es = td};
 
-    // Mat to = {
-    //     .rows = n,
-    //     .cols = 1,
-    //     .stride = stride,
-    //     .es = td + 2};
     Mat data = {
         .cols = 3,
         .rows = 4,
