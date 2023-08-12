b9059871f3bcef02ef6efcd2bdf5d635cb905c01
Yurii Cherniak <juracherniak@gmail.com>
2023-08-12 12:13:49 +0300

    clear things up

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ exec=img2nn
 
 rm $exec 
 
-gcc -o $exec $exec.c -Wall -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+gcc -o build/$exec demos/$exec.c -Wall -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 
 # ./$exec
 # ./$exec xor.arch xor.mat
 # ./$exec img.arch img.mat
-./$exec mnist/8.png mnist/6.png
+./build/$exec mnist/8.png mnist/6.png
